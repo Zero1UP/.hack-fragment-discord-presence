586dd1b44a187b05dcb6dbc0f22bb8e04edd5a5d
1UP <zero1up@gmail.com>
2020-10-30 07:13:34 -0400

    Removed memory.dll

--- fragmentDiscordPresence/Helpers/GameHelper.cs
+++ fragmentDiscordPresence/Helpers/GameHelper.cs
@@ -8,12 +8,12 @@ namespace fragmentDiscordPresence
 {
     public static class GameHelper
     {
-        public const string CONNECTED_TO_AS_ADDRESS = "206F92F0";
-        public const string AREA_WORD_POINTER = "206F9438";
-        public const string SERVER_NAME_ADDRESS = "206F94F2";
-        public const string PARTY_COUNT_ADDRESS = "208B93A8";
-        public const string IN_AREA_ADDRESS = "20877DA8"; 
-        public const string ZONE_CCS_FILE_LOADED = "206541F4";
+        public static IntPtr CONNECTED_TO_AS_ADDRESS = new IntPtr(0x206F92F0);
+        public static IntPtr AREA_WORD_POINTER = new IntPtr(0x206F9438);
+        public static IntPtr SERVER_NAME_ADDRESS =new IntPtr(0x206F94F2);
+        public static IntPtr PARTY_COUNT_ADDRESS = new IntPtr(0x208B93A8);
+        public static IntPtr IN_AREA_ADDRESS = new IntPtr(0x20877DA8); 
+        public static IntPtr ZONE_CCS_FILE_LOADED = new IntPtr(0x206541F4);
         public const int AREA_WORD_ONE_POINTER_OFFSET= 4;
         public const int AREA_WORD_TWO_POINTER_OFFSET = 8;
         public const int AREA_WORD_THREE_POINTER_OFFSET = 12;
--- fragmentDiscordPresence/frm_Main.cs
+++ fragmentDiscordPresence/frm_Main.cs
@@ -1,15 +1,17 @@
-﻿using DiscordRPC;
-using Memory;
+﻿using Binarysharp.MemoryManagement;
+using DiscordRPC;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace fragmentDiscordPresence
 {
@@ -17,13 +19,13 @@ namespace fragmentDiscordPresence
     {
         private const string PCSX2PROCESSNAME = "pcsx2";
         bool pcsx2Running = false;
-        Mem m = new Mem();
+        MemorySharp m = null;
         bool gameStarted = false;
         public DiscordRpcClient client;
         private static RichPresence presence = new RichPresence()
         {
-            Details = "Not currently in an area.",
-            State = "Not currently in a server.",
+            Details = "Not currently in a server.",
+            State = "Not currently in an area.",
             Assets = new Assets()
             {
                 LargeImageKey = "default",
@@ -63,75 +65,83 @@ namespace fragmentDiscordPresence
         {
             if(pcsx2Running)
             {
-                m.OpenProcess(PCSX2PROCESSNAME + ".exe");
+                m = new MemorySharp(Process.GetProcessesByName(PCSX2PROCESSNAME).First());
 
                 //Check to make sure we are in an Area server.
-                if (m.readByte(GameHelper.CONNECTED_TO_AS_ADDRESS) == 1 )
+                try
                 {
-                    //Get party count
-                    //Not really useful right now since I can't seem to get this to display
-                    int partyCount = m.readByte(GameHelper.PARTY_COUNT_ADDRESS);
-                    
-                    //Get the server name
-                    string serverName = ByteConverstionHelper.converyBytesToSJIS(m.readBytes(GameHelper.SERVER_NAME_ADDRESS, 20));
-                    
-                    //Get the town you are in.
-                    //This address actually shows the current zone ccs file. So when you zone into an area this will update to that zone's CCS file
-                    //Could be useful to display the area you are in but using the default assets wouldn't be telling of where the player actually is.
-                    string townFileName = ByteConverstionHelper.convertBytesToString(m.readBytes(GameHelper.ZONE_CCS_FILE_LOADED, 12)).Split('.')[0].ToLower();
-
-                    //Don't want the timer to continue if the player is just sitting in town.
-                    if(townFileName.ToLower().Contains("town"))
+                    if (m.Read<byte>(GameHelper.CONNECTED_TO_AS_ADDRESS, 4, false).First() == 0x01)
                     {
-                        presence.Timestamps = new Timestamps()
-                        {
-                           Start = null
+                        //Get party count
+                        //Not really useful right now since I can't seem to get this to display
+                        int partyCount = m.Read<byte>(GameHelper.PARTY_COUNT_ADDRESS, false);
 
-                        };
-                        setPresence(serverName, "", "", "", partyCount, townFileName);
-                        gameStarted = false;
-                    }
-                   
-                    //Check to see if we are in a zone. (Could technically use townFileName, but I haven't don't extensive testing with that to see if it is
-                    // always a zone file name that is loaded there.)
-                    if (m.readByte(GameHelper.IN_AREA_ADDRESS) == 1)
-                    {
-                        //Get the pointer addresses and add their offsets to them
-                        int areaword1Pointer = int.Parse(ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(GameHelper.AREA_WORD_POINTER, 4)), System.Globalization.NumberStyles.HexNumber) + GameHelper.AREA_WORD_ONE_POINTER_OFFSET;
-                        int areaWord2Pointer = int.Parse(ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(GameHelper.AREA_WORD_POINTER, 4)), System.Globalization.NumberStyles.HexNumber) + GameHelper.AREA_WORD_TWO_POINTER_OFFSET;
-                        int areaWord3Pointer = int.Parse(ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(GameHelper.AREA_WORD_POINTER, 4)), System.Globalization.NumberStyles.HexNumber) + GameHelper.AREA_WORD_THREE_POINTER_OFFSET;
+                        //Get the server name
+                        string serverName = ByteConverstionHelper.converyBytesToSJIS(m.Read<byte>(GameHelper.SERVER_NAME_ADDRESS, 20, false));
 
-                        //Convert them to the useable address (ex. 20000000)
-                        string areaWord1PointerAddress = ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(areaword1Pointer.ToString("X4"), 4));
-                        string areaWord2PointerAddress = ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(areaWord2Pointer.ToString("X4"), 4));
-                        string areaWord3PointerAddress = ByteConverstionHelper.byteArrayHexToAddressString(m.readBytes(areaWord3Pointer.ToString("X4"), 4));
+                        //Get the town you are in.
+                        //This address actually shows the current zone ccs file. So when you zone into an area this will update to that zone's CCS file
+                        //Could be useful to display the area you are in but using the default assets wouldn't be telling of where the player actually is.
+                        string townFileName = ByteConverstionHelper.convertBytesToString(m.Read<byte>(GameHelper.ZONE_CCS_FILE_LOADED, 12, false)).Split('.')[0].ToLower();
 
-                        //Get the keywords.
-                        string areaWord1 = ByteConverstionHelper.converyBytesToSJIS(m.readBytes(areaWord1PointerAddress, 12)).Substring(4);
-                        string areaWord2 = ByteConverstionHelper.converyBytesToSJIS(m.readBytes(areaWord2PointerAddress, 12)).Substring(4);
-                        string areaWord3 = ByteConverstionHelper.converyBytesToSJIS(m.readBytes(areaWord3PointerAddress, 12)).Substring(4);
-
-                        //Check to see if the game is started (if the player is in a zone) so the timer won't constantly reset
-                        if(!gameStarted)
+                        //Don't want the timer to continue if the player is just sitting in town.
+                        if (townFileName.ToLower().Contains("town"))
                         {
                             presence.Timestamps = new Timestamps()
                             {
-                                Start = DateTime.UtcNow
+                                Start = null
 
                             };
-
-                            gameStarted = true;
+                            setPresence(serverName, "", "", "", partyCount, townFileName);
+                            gameStarted = false;
                         }
-                        //Set their discord presence
-                        setPresence(serverName, areaWord1, areaWord2, areaWord3, partyCount, "default");
+
+                        //Check to see if we are in a zone. (Could technically use townFileName, but I haven't done extensive testing with that to see if it is
+                        // always a zone file name that is loaded there.)
+
+                        if (m.Read<byte>(GameHelper.IN_AREA_ADDRESS, 4, false).First() == 0x01)
+                        {
+                            //Get the pointer addresses and add their offsets to them
+                            IntPtr areawordPointerAddress = new IntPtr(Convert.ToInt32(ByteConverstionHelper.byteArrayHexToAddressString(m.Read<byte>(GameHelper.AREA_WORD_POINTER, 4, false)), 16));
+
+
+                            IntPtr areaword1Pointer = new IntPtr(Convert.ToInt32(ByteConverstionHelper.byteArrayHexToAddressString(m.Read<byte>(areawordPointerAddress + GameHelper.AREA_WORD_ONE_POINTER_OFFSET, 4, false)), 16));
+                            IntPtr areaWord2Pointer = new IntPtr(Convert.ToInt32(ByteConverstionHelper.byteArrayHexToAddressString(m.Read<byte>(areawordPointerAddress + GameHelper.AREA_WORD_TWO_POINTER_OFFSET, 4, false)), 16));
+                            IntPtr areaWord3Pointer = new IntPtr(Convert.ToInt32(ByteConverstionHelper.byteArrayHexToAddressString(m.Read<byte>(areawordPointerAddress + GameHelper.AREA_WORD_THREE_POINTER_OFFSET, 4, false)), 16));
+
+                            //Get the keywords.
+                            string areaWord1 = ByteConverstionHelper.converyBytesToSJIS(m.Read<byte>(areaword1Pointer, 12, false)).Substring(4);
+                            string areaWord2 = ByteConverstionHelper.converyBytesToSJIS(m.Read<byte>(areaWord2Pointer, 12, false)).Substring(4);
+                            string areaWord3 = ByteConverstionHelper.converyBytesToSJIS(m.Read<byte>(areaWord3Pointer, 12, false)).Substring(4);
+
+                            //Check to see if the game is started (if the player is in a zone) so the timer won't constantly reset
+                            if (!gameStarted)
+                            {
+                                presence.Timestamps = new Timestamps()
+                                {
+                                    Start = DateTime.UtcNow
+
+                                };
+
+                                gameStarted = true;
+                            }
+                            //Set their discord presence
+                            setPresence(serverName, areaWord1, areaWord2, areaWord3, partyCount, "default");
+
+                        }
 
                     }
-
+                    else
+                    {
+                        setPresence("", "", "", "", 0, "default");
+                    }
                 }
-                else
+                catch (Exception)
                 {
+
                     setPresence("", "", "", "", 0, "default");
                 }
+  
             }
         }
         private void setPresence(string serverName, string word1, string word2, string word3, int playerCount,string zone)
@@ -139,20 +149,20 @@ namespace fragmentDiscordPresence
             //Only checking the first word. If this isn't set then it's save to assume that we weren't in a zone
             if(word1 =="")
             {
-                presence.Details = "Not currently in an area.";
+                presence.State = "Not currently in an area.";
             }
             else
             {
-                presence.Details = "Area: " + word1 + " " + word2 + " " + word3;
+                presence.State = "Area: " + word1 + " " + word2 + " " + word3;
             }
            
             if(serverName == "")
             {
-                presence.State = "Not currently in a server.";
+                presence.Details = "Not currently in a server.";
             }
             else
             {
-                presence.State = "Area Server: " + serverName;
+                presence.Details = "Area Server: " + serverName;
             }
 
             presence.Assets = new Assets();
