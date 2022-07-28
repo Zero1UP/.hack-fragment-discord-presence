95b1c151edc51f307dd259a575bf9e9228c7c6de
1UP <zero1up@gmail.com>
2022-07-28 00:10:44 -0400

    Updated assembly version to reflect 64bit pcsx2 check Updated packages

--- fragmentDiscordPresence/frm_Main.cs
+++ fragmentDiscordPresence/frm_Main.cs
@@ -1,17 +1,10 @@
 ﻿using Binarysharp.MemoryManagement;
 using DiscordRPC;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 namespace fragmentDiscordPresence
 {
